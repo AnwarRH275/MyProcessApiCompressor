e293e52f9b8761f52b8325d6ec6608d91747958e
AnwarRH275 <erhanianwar@gmail.com>
2021-12-09 09:52:57 +0100

    first commit

---
+++ MyProcessCompressAPI.py
@@ -0,0 +1,133 @@
+#app.py
+from flask import Flask, json, request, jsonify,send_from_directory
+import os
+import urllib.request
+from werkzeug.utils import secure_filename
+import sys
+from PDFNetPython3.PDFNetPython import PDFDoc, Optimizer, SDFDoc, PDFNet
+ 
+app = Flask(__name__)
+ 
+
+app.secret_key = "MyProcessCompressor.BPM24"
+ 
+UPLOAD_FOLDER = 'UPLOAD_FOLDER'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+# Code Compressor API
+def get_size_format(b, factor=1024, suffix="B"):
+    """
+    Scale bytes to its proper byte format
+    e.g:
+
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
+
+def compress_file(input_file: str, output_file: str):
+    """Compress PDF file"""
+    if not output_file:
+        output_file = input_file
+    initial_size = os.path.getsize(input_file)
+    try:
+        # Initialize the library
+        PDFNet.Initialize()
+        doc = PDFDoc(input_file)
+        # Optimize PDF with the default settings
+        doc.InitSecurityHandler()
+        # Reduce PDF size by removing redundant information and compressing data streams
+        Optimizer.Optimize(doc)
+        doc.Save(UPLOAD_FOLDER+'/'+output_file, SDFDoc.e_linearized)
+        doc.Close()
+    except Exception as e:
+        print("Error compress_file=", e)
+        doc.Close()
+        return False
+    compressed_size = os.path.getsize(output_file)
+    ratio = 1 - (compressed_size / initial_size)
+    summary = {
+        "Input File": input_file, "Initial Size": get_size_format(initial_size),
+        "Output File": output_file, f"Compressed Size": get_size_format(compressed_size),
+        "Compression Ratio": "{0:.3%}.".format(ratio)
+    }
+    # Printing Summary
+    print("## Summary ########################################################")
+    print("\n".join("{}:{}".format(i, j) for i, j in summary.items()))
+    summary = " --------  ".join("{}:{}".format(i, j) for i, j in summary.items())
+    print("###################################################################")
+    return summary
+
+def archiveFile(input_file_name,output_file_name):
+    summary = compress_file(UPLOAD_FOLDER+'/'+input_file_name,UPLOAD_FOLDER+'/'+ output_file_name)
+    
+    return summary
+    
+# Code API Compressor
+
+ 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+ 
+@app.route('/')
+def main():
+    return 'Hello To MyProcess Api Compressor'
+
+
+
+ 
+@app.route('/api', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+ 
+    files = request.files.getlist('file')
+    
+    errors = {}
+    success = False
+     
+    for file in files:      
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            success = True
+            summary = archiveFile(filename,filename[:-4]+'_min.pdf')
+        else:
+            errors[file.filename] = 'File type is not allowed'
+    
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp = jsonify({'message' : 'Files successfully uploaded',
+                         'summary':summary,
+                         'url':'http://127.0.0.1:5000/'+UPLOAD_FOLDER+'/'+filename[:-4]+'_min.pdf'
+        })
+        ##download_file(filename[:-4]+'-min.pdf')
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    
+@app.route('/UPLOAD_FOLDER/<path:filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename, as_attachment=True)
+ 
+if __name__ == '__main__':
+    port = os.environ.get("PORT",5000)
+    app.run(debug=True,host="0.0.0.0",port=port)
